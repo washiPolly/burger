ef9a6550ccbb0abe4829fe89cc3bfefb05105eef
washiPolly <pollylsu@gmail.com>
2020-05-09 13:48:01 -0700

    added schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Protein Style Burger');
+INSERT INTO burgers (burger_name) VALUES ('Popeyes Spicy Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chic-fil-a Original Spicy Chicken Burger');
