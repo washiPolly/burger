cbc0d7bdaa60dc0a412c6c5a375d8d749167a888
washiPolly <pollylsu@gmail.com>
2020-05-11 01:13:27 -0700

    retrying connection var

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,12 @@ if (process.env.JAWSDB_URL) {
 }else {
   connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  // port: 3306,
   user: "root",
-  password: "pollyServer1",
-  database: "burgers_db"
+  // password: "pollyServer1",
+  // database: "burgers_db"
+  password: "hackthe planet",
+  database: "todoagain_db"
 });
 
 // Make connection.
